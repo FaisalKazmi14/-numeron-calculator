ebb19d30d53ab7cffb95a27da9525ac6e317ab50
FaisalKazmi14 <faisalkazmi512147272@gmail.com>
2026-05-14 17:15:04 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'numeron-v5';
+const CACHE_NAME = 'numeron-v10';
 const ASSETS = [
   './',
   './index.html',
@@ -6,30 +6,14 @@ const ASSETS = [
   './logo.png'
 ];
 
-// Install Event
 self.addEventListener('install', (e) => {
-  self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-// Activate Event
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(keys.map((key) => {
-        if (key !== CACHE_NAME) return caches.delete(key);
-      }));
-    })
-  );
-});
-
-// Fetch Event
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((res) => {
-      return res || fetch(e.request);
-    })
+    caches.match(e.request).then((res) => res || fetch(e.request))
   );
 });
