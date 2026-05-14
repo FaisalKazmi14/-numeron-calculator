17732ec53f7de71113b327883af81f3c8867cb01
FaisalKazmi14 <faisalkazmi512147272@gmail.com>
2026-05-15 01:00:28 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,19 +1,22 @@
-const CACHE_NAME = 'numeron-v10';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './logo.png'
-];
+﻿const CACHE = 'numeron-premium-v1';
+const ASSETS = ['./', './index.html', './manifest.json', './logo.png'];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
-  );
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request).then(r => {
+    if (r.status === 200) {
+      const clone = r.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+    }
+    return r;
+  }).catch(() => caches.match('./index.html'))));
 });
