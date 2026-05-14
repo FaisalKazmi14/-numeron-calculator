21a39f82bc86bfa430a32a00709133ab45fe9c73
FaisalKazmi14 <faisalkazmi512147272@gmail.com>
2026-05-14 16:07:23 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'numeron-v5';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './logo.png'
+];
+
+// Install Event
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Activate Event
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(keys.map((key) => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }));
+    })
+  );
+});
+
+// Fetch Event
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
+    })
+  );
+});
